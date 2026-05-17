06e93f78959854f73d3ce3c0b4537c7b68dac455
LariKaroll <larikarol108@gmail.com>
2026-05-17 12:13:35 -0300

    feat: add inicializer firebase

---
+++ backend/src/main/java/br/com/vollun/config/FirebaseConfig.java
@@ -0,0 +1,39 @@
+package br.com.vollun.config;
+
+import br.com.vollun.exceptions.RecursoNaoEncontradoException;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+@Configuration
+public class FirebaseConfig {
+
+    public void initialize() {
+        try {
+            InputStream serviceAccount = getClass()
+                    .getClassLoader()
+                    .getResourceAsStream("firebase-chave.json");
+
+            if(serviceAccount == null) {
+                throw new RecursoNaoEncontradoException("Arquivo firebase-chave.json não foi encontrado na pasta resources!");
+            }
+
+            FirebaseOptions option = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setStorageBucket("project---vollun.firebasestorage.app")
+                    .build();
+
+            if(FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(option);
+                System.out.println("Firebase inicializado com sucesso!");
+            }
+        } catch (RecursoNaoEncontradoException | IOException e) {
+            System.err.println("Erro ao inicializar o Firebase" + e.getMessage());
+        }
+    }
+}
